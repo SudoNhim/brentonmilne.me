03d1e2150b9eef8c980dc4240937ce561703d548
Brenton Milne <brenton.a.milne@gmail.com>
2015-03-31 21:02:38 +1300

    Added menu

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { testcontent: 'some content' });
+  res.render('index', {menu: ['about', 'image-code', 'minecraft'], testcontent: 'some content' });
 });
 
 module.exports = router;
