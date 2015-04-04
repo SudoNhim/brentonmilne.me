5056cbb5754ef9e16991f9ec240a496e721b816a
Brenton Milne <brenton.a.milne@gmail.com>
2015-04-04 14:39:21 +1300

    oops - forgot to add

---
+++ routes/toplevel_pages.js
@@ -0,0 +1,34 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var appdata = {
+  menu: [
+    {
+      title: 'about', 
+      thumbnail: 'images/menu_thumbs/about_thumb.png',
+      link: '/'
+    }, 
+    {
+      title: 'image live-coder', 
+      thumbnail: 'images/menu_thumbs/image_code_thumb.png',
+      link: '/imagecode'
+    }, 
+    {
+      title: 'minecraft raytracer', 
+      thumbnail: 'images/menu_thumbs/minecraft_raytracer_thumb.png',
+      link: '/minecraft'
+    }]
+}
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.render('index', { appdata: appdata });
+});
+
+
+router.get('/minecraft', function (req, res) {
+  res.render('minecraft_raytracer', { appdata: appdata });
+});
+
+
+module.exports = { router: router, appdata: appdata };
